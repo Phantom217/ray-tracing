f19d368d012837120308aadcbb9e7aaa20b33efd
Tyler Oalman <tyleroalman@gmail.com>
2022-04-08 18:49:03 -0500

    Create `World` type to store list of hittable objects

--- src/hit.rs
+++ src/hit.rs
@@ -22,3 +22,21 @@ impl HitRecord {
 pub trait Hit {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
 }
+
+pub type World = Vec<Box<dyn Hit>>;
+
+impl Hit for World {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut tmp_hr = None;
+        let mut closest_so_far = t_max;
+
+        for object in self {
+            if let Some(hr) = object.hit(ray, t_min, closest_so_far) {
+                closest_so_far = hr.t;
+                tmp_hr = Some(hr);
+            }
+        }
+
+        tmp_hr
+    }
+}
